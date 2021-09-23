06bcd6cb2912d4fac9c1993c1bb19d64e9325007
Sean Spence <sean.spence@students.mq.edu.au>
2021-09-23 16:49:10 +1000

    Initial commit

--- ohwhaley.R
+++ ohwhaley.R
@@ -63,8 +63,7 @@ devtools::load_all()
 say()
 
 devtools::document()
-?say
-??say
+
 usethis::use_package("glue")
 
 usethis::use_testthat()
@@ -72,5 +71,27 @@ usethis::use_test("say")
 
 ?say
 
+devtools::install_github()
+
+usethis::use_git_config(user.name = "Sean Spence",
+                        user.email = "sean.spence@students.mq.edu.au")
+usethis::create_github_token()
+
+install.packages("gitcreds")
+library("gitcreds")
+
+gitcreds::gitcreds_set()
+
+gh::gh_whoami()
+
+usethis::git_sitrep()
+?git_vaccinate
+
+usethis::use_git()
+usethis::use_github()
+
+usethis::use_readme_rmd()
+
+usethis::git_vaccinate()
 
 
